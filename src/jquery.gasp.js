a38485f4e3e7f6c7a6eb398251622957d2c7e003
wilsonhut <github@wilsonhut.com>
2013-06-13 16:04:32 -0400

    Create jquery.gasp.js

---
+++ src/jquery.gasp.js
@@ -0,0 +1,54 @@
+;(function ($, undefined) {
+    var slice = Array.prototype.slice;
+    $.defer = function ( /*[func] [, delay] [, funcParameters]*/ ) {
+        var args = arguments,
+            isFunc = $.isFunction(args[0]),
+            func = isFunc ? args[0] : $.noop,
+            delay = (isFunc ? args[1] : args[0]) || 0,
+            funcArgs = isFunc ? slice.call(args, 2) : undefined,
+            isCancelled = false,
+            cancel = function (reject /* = true */ ) {
+                clearTimeout(timerId);
+                isCancelled = true;
+                if ((!arguments.length || reject) && deferred.state() === "pending") {
+                    deferred.rejectWith(null, funcArgs);
+                }
+            },
+            deferred = $.Deferred(),
+            timerId = setTimeout(function () {
+                deferred.notifyWith(promise, funcArgs);
+                if (isCancelled) {
+                    return;
+                }
+                try {
+                    var result = func.apply(this, funcArgs);
+                    deferred.resolveWith(result, funcArgs);
+                } catch (e) {
+                    deferred.rejectWith(e, funcArgs);
+                }
+            }, delay),
+            promise = $.extend(deferred.promise(), {
+                cancel: cancel
+            });
+        return promise;
+    };
+})(jQuery);
+;(function ($, undefined) {
+    var defaults = {
+        breathTime: 0
+    };
+
+    $.fn.gasp = function (func, options) {
+        options = $.extend({}, defaults, options);
+        var promise = $.when();
+        this.each(function () {
+            var self = this;
+            promise = promise.then(function () {
+                return $.defer(function (el) {
+                    func.call(el);
+                }, options.breathTime, self);
+            });
+        });
+        return promise;
+    };
+})(jQuery);
