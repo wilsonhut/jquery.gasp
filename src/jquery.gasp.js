e3a457fccb2eb235c15b3d44174f9976c18ed075
wilsonhut <github@wilsonhut.com>
2013-06-20 09:48:40 -0400

    Updated $.defer. Added 'wait' to the options. This is the option for how long to wait before doing the first promise.

--- src/jquery.gasp.js
+++ src/jquery.gasp.js
@@ -1,52 +1,56 @@
-;(function ($, undefined) {
-    var slice = Array.prototype.slice;
-    $.defer = function ( /*[func] [, delay] [, funcParameters]*/ ) {
-        var args = arguments,
-            isFunc = $.isFunction(args[0]),
-            func = isFunc ? args[0] : $.noop,
-            delay = (isFunc ? args[1] : args[0]) || 0,
-            funcArgs = isFunc ? slice.call(args, 2) : undefined,
-            isCancelled = false,
-            cancel = function (reject /* = true */ ) {
-                clearTimeout(timerId);
-                isCancelled = true;
-                if ((!arguments.length || reject) && deferred.state() === "pending") {
-                    deferred.rejectWith(null, funcArgs);
-                }
-            },
-            deferred = $.Deferred(),
-            timerId = setTimeout(function () {
-                deferred.notifyWith(promise, funcArgs);
-                if (isCancelled) {
-                    return;
-                }
-                try {
-                    var result = func.apply(this, funcArgs);
-                    deferred.resolveWith(result, funcArgs);
-                } catch (e) {
-                    deferred.rejectWith(e, funcArgs);
-                }
-            }, delay),
-            promise = $.extend(deferred.promise(), {
-                cancel: cancel
-            });
-        return promise;
-    };
+;(function($) {
+	var slice = Array.prototype.slice;
+	$.defer = function( /*[func] [, delay] [, funcParameters]*/) {
+		var args = arguments,
+		    isFunc = $.isFunction(args[0]),
+		    func = isFunc ? args[0] : $.noop,
+		    delay = (isFunc ? args[1] : args[0]) || 0,
+		    funcArgs = isFunc ? slice.call(args, 2) : [],
+		    isCancelled = false,
+		    cancel = function(reject /* = true */) {
+			    clearTimeout(timerId);
+			    isCancelled = true;
+			    if ((!arguments.length || reject) && deferred.state() === "pending") {
+				    deferred.rejectWith(null, funcArgs);
+			    }
+		    },
+		    deferred = $.Deferred(),
+		    timerId = setTimeout(function() {
+			    deferred.notifyWith(promise, funcArgs);
+			    if (isCancelled) {
+				    return;
+			    }
+			    try {
+				    var result = func.apply(this, funcArgs);
+				    deferred.resolveWith(result, funcArgs);
+			    } catch(e) {
+				    deferred.rejectWith(e, funcArgs);
+			    }
+		    }, delay),
+		    promise = $.extend(deferred.promise(), {
+			    cancel: cancel
+		    });
+		return promise;
+	};
 })(jQuery);
 ;(function ($) {
     var defaults = {
-		breathTime: 0
+		breathTime: 0,
+		wait: 0
 	};
 
 	$.fn.gasp = function (func, options, promise) {
 		options = $.extend({}, defaults, options);
 		promise = promise || $.when();
+		var isFirst = true;
 		this.each(function () {
 			var self = this;
 			promise = promise.then(function () {
-				return $.defer(function (el) {
+				var p = $.defer(function (el) {
 					func.call(el);
-				}, options.breathTime, self);
+				}, (isFirst? options.wait : options.breathTime), self);
+				isFirst = false;
+				return p;
 			});
 		});
 		return promise;
